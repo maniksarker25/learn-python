412b01b8fb4fa7ebab524ebdb6426889de6c99c4
manik <maniksarker652@gmail.com>
2025-12-26 16:23:27 +0600

    learn for loop and range

---
+++ for.py
@@ -0,0 +1,24 @@
+numbers = [5,10,15,20,25]
+
+# for
+# sum = 0
+# for num in numbers:
+#     print(num)
+#     sum = sum + num
+
+# print(sum)
+
+
+# text = "Pagla hawar"
+# for char in text:
+#     print(char)
+
+# range
+# print(range(1,11))
+
+# for i in range(1,11):
+#     print(i)
+
+# with steps
+for i in range(1,11,2):
+    print(i)
