4f9c562dae8248fe70acbedd56d382959398efcb
manik <maniksarker652@gmail.com>
2026-03-29 19:41:46 +0600

    learn about basic funtion

---
+++ funtion.py
@@ -0,0 +1,15 @@
+# define
+
+def doubel_it(num):
+    result = num *2
+    print(result)
+
+doubel_it(8)
+doubel_it(12)
+
+def sum(num1,num2):
+    result = num1 + num2
+    return result
+
+total = sum(12,28)
+print(total)
