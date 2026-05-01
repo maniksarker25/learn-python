b0461ca9cf641e0f91b45dc39a7b370333886cc1
manik <maniksarker652@gmail.com>
2026-05-01 23:55:11 +0600

    learn about build in method

---
+++ build_in_modules.py
@@ -0,0 +1,12 @@
+from math import *
+from random import *
+from time import sleep
+# result = ceil(3.4)
+
+# print(result)
+
+# print(random())
+# print(randint(1,100))
+
+sleep(4)
+print(choice(["manik","raju","billal","setu","sanoar"]))
