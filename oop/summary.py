342e1ff5cafb3389ab2e839364f8eb6b0de2ab86
manik <maniksarker652@gmail.com>
2026-05-05 23:19:33 +0600

    learn more about oop

---
+++ oop/summary.py
@@ -0,0 +1,20 @@
+class Book:
+    def __init__(self,name):
+        self.name = name
+    def read(self):
+        raise NotImplementedError
+
+class Physics(Book):
+    def __init__(self, name,lab):
+        self.lab = lab
+        super().__init__(name)
+    def read(self):
+        print("Reading physics vector chapter")
+
+        
+topon = Physics("topon",True)
+print(issubclass(Physics,Book))
+print(isinstance(topon,Physics))
+print(isinstance(topon,Book))
+
+topon.read()
